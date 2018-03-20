7d1a781935a77b7eb5398bdda5b79689e3c40355
Vitalii Kushnir <kusha.apple@gmail.com>
2018-03-20 21:34:34 +0100

    wonderful-quotes-app

---
+++ wonderful-quotes-app/README.md
@@ -0,0 +1,16 @@
+# wonderful-quotes-app
+
+> A small Vue.js application which uses Vue components for building SPA.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ wonderful-quotes-app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <new-quote @quoteAdded="newQuote"></new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a quote to delete it
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import AppHeader from './components/Header.vue';
+
+  export default {
+    data: function () {
+      return {
+        maxQuotes: 10,
+        quotes: ["Just a Quote"]
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      newQuote: NewQuote,
+      appHeader: AppHeader
+    },
+    methods: {
+      newQuote(quote) {
+        if (this.quotes >= 10) {
+          alert("Please delete quote first");
+        }
+        this.quotes.push(quote)
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
