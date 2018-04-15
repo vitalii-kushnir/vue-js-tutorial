72c6d34bcc4a72c1678dad071f30d31710ddcf18
Vitalii Kushnir <kusha.apple@gmail.com>
2018-04-15 13:13:55 +0200

    stock-trader-app: connecting the potfolio with vuex

--- stock-trader-app/src/components/portfolio/Portfolio.vue
+++ stock-trader-app/src/components/portfolio/Portfolio.vue
@@ -1,9 +1,21 @@
 <template>
-    <h1>portfolio</h1>
+    <div>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
 </template>
 
 <script>
-</script>
+  import {mapGetters} from 'vuex';
+  import Stock from './Stock.vue';
 
-<style>
-</style>
+  export default {
+    computed: {
+      ...mapGetters({
+        stocks: 'stockPortfolio'
+      })
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
