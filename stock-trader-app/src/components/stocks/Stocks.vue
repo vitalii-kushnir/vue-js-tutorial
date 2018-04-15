85c4ce2777fef61b56aa0ef1f4ccfb90219dbda1
Vitalii Kushnir <kusha.apple@gmail.com>
2018-04-15 11:53:28 +0200

    stock-trader-app: creating stocks components

--- stock-trader-app/src/components/stocks/Stocks.vue
+++ stock-trader-app/src/components/stocks/Stocks.vue
@@ -1,8 +1,27 @@
 <template>
-    <h1>-stocks</h1>
+    <div>
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>
 </template>
 
 <script>
+  import Stock from './Stock.vue';
+
+  export default {
+    components: {
+      appStock: Stock
+    },
+
+    data() {
+      return {
+        stocks : [
+          {id:1, name: "bmw", price : 22 },
+          {id:2, name: "audi", price : 33 },
+          {id:3, name: "vw", price : 44 }
+        ]
+      }
+    }
+  }
 </script>
 
 <style>
