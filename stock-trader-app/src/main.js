c74f81eb3c78480b09470aa846e3e739b7e6c28b
Vitalii Kushnir <kusha.apple@gmail.com>
2018-04-16 20:07:14 +0200

    stock-trader-app: adding interaction with backend

--- stock-trader-app/src/main.js
+++ stock-trader-app/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
 import { routes } from './routes';
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-tutorial.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
